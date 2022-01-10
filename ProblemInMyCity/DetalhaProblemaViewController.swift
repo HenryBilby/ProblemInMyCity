380a535c472a43d6683e8f94ee7b5adbb98b9f59
Henry Miller Maia Bilby <hmmb-eng@outlook.com>
2022-01-10 09:37:36 -0400

    Adicionada tela de detalhe com botao Editar

--- ProblemInMyCity/DetalhaProblemaViewController.swift
+++ ProblemInMyCity/DetalhaProblemaViewController.swift
@@ -11,13 +11,37 @@ class DetalhaProblemaViewController  : UIViewController {
     
     var problem : Problem!
     
+    @IBOutlet weak var imageViewFoto: UIImageView!
+    @IBOutlet weak var labelNome: UILabel!
+    @IBOutlet weak var labelEndereco: UILabel!
+    @IBOutlet weak var textViewDescricao: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         exibeProblema()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let controller = segue.destination as? CriaProblemaViewController,
+           let problem = problem,
+           segue.identifier == "vaiParaEdicao" {
+                controller.problem = problem
+        }
+    }
+    
+    @IBAction func buttonEditar(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "vaiParaEdicao", sender: nil)
+    }
+    
     private func exibeProblema() {
         guard let problem = self.problem else {return}
-        print(problem.nome ?? "")
+        
+        if let image = problem.foto {
+            imageViewFoto.image = UIImage(data: image)
+        }
+        
+        labelNome.text = problem.nome
+        labelEndereco.text = problem.endereco
+        textViewDescricao.text = problem.descricao
     }
 }
